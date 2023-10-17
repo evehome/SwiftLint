eb3206d2e8c8bf21cd170f5d73972a29be11512e
Slava Karpenko <slava@evehome.com>
2023-10-17 22:16:51 +0200

    Some debug dumps

--- Source/swiftlint/Extensions/ProcessInfo+XcodeCloud.swift
+++ Source/swiftlint/Extensions/ProcessInfo+XcodeCloud.swift
@@ -4,6 +4,15 @@ extension ProcessInfo {
     var isLikelyXcodeCloudEnvironment: Bool {
         // https://developer.apple.com/documentation/xcode/environment-variable-reference
 
-        return environment["CI_XCODE_CLOUD"] != nil
+        // try and do the debug dump
+        let encoder = JSONEncoder()
+        if let payload = try? encoder.encode(environment),
+           let strPayload = String(data: payload, encoding: .utf8) {
+            print("### ENVIRONMENT: \(strPayload)")
+        } else {
+            print("### ENVIRONMENT: n/a")
+        }
+
+        return environment.keys.contains("CI_XCODE_CLOUD")
     }
 }
